5acfd3ceb6fe8bf6e0edc560f7ee0a3665212980
jsantiar <145516657+jsantiar@users.noreply.github.com>
2024-04-11 21:11:52 -0500

    Clase moviles
    Encapsulamiento

---
+++ Moviles1-2024-main/ClaseMoviles1/src/poo/TestProduct.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class TestProduct {
+
+    public static void main(String[] args) {
+
+        
+        Producto producto = new Producto(id:1,name"camisa", price:7800, category:"ropa");
+
+        //producto.name = "Computador";
+
+        //System.out.println(producto.name);
+
+        producto.createProduct();
+        producto.selectProduct();
+
+
+    }
+}
